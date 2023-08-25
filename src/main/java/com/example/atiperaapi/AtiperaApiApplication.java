8f6b00ba53f981f38876929ce748ca40b75bd321
Marysia Antoniak <marysiaantoniak01@gmail.com>
2023-08-25 11:00:45 +0200

    Initial commit

---
+++ src/main/java/com/example/atiperaapi/AtiperaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.atiperaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtiperaApiApplication.class, args);
+    }
+
+}
